1356bde6ae9bf137ba9529c9baea7dd2b5438bd3
Xinyi Wang <is.xinyiwang@outlook.com>
2024-04-08 11:13:31 +0200

    setup for github page hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {    
+    output: 'export', // Enable static exports
+    images: {
+        unoptimized: true, // Disable image optimization for GitHub Pages
+    },
+    basePath: '/cassidy',
+};
 
 export default nextConfig;
